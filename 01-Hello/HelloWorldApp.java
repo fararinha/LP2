b7d8e643379add371224fd0ab3652524faaee0ba
fararinha <yehaw045@gmail.com>
2026-03-10 20:43:23 -0300

    Add files via upload

--- 01-Hello/HelloWorldApp.java
+++ 01-Hello/HelloWorldApp.java
@@ -1,9 +1,6 @@
-//HelloWorldApp.java
-class HelloWorldApp{
-	public static void main (String[] args){
-	System.out.println("Hello world");
-	}
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
 }
-
-
-
